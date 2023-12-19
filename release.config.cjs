8f3ae606ae97c9202c3187eb6ef94de03626e944
Sebastian <sebastian@starshive.com>
2023-12-19 10:31:10 -0300

    CR61

--- release.config.cjs
+++ release.config.cjs
@@ -2,21 +2,10 @@ module.exports = {
     branches: ['main'],
     tagFormat: 'v${version}',
     plugins:[
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
-        [
-            '@semantic-release/github',
-            {
-                successComment: false,
-                failComment: false,
-            },
-        ],
-        [
-            '@semantic-release/git',
-            {
-                message: 'chore(release): ${nextRelease.gitTag} [skip ci]',
-            }
-        ]
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/changelog",
+        "@semantic-release/npm",
+        "@semantic-release/git",
+        "@semantic-release/github"
     ]
 };
